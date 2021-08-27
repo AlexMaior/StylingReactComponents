c066c8bf13a559e9b612eb700834a55820bcc6a0
Alex Maior <alexmaior@yahoo.com>
2021-08-27 12:15:33 +0300

    After clicking the Add Goal button input is emptied

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -50,6 +50,7 @@ const CourseInput = (props) => {
       return;
     }
     props.onAddGoal(enteredValue);
+    setEnteredValue("");
   };
 
   return (
@@ -58,7 +59,11 @@ const CourseInput = (props) => {
         className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
       >
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
